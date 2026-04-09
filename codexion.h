2d230551ef860bb0883ea461bbaa4e3459439270
Nuno Street <nuno.dslemos@gmail.com>
2026-04-09 17:50:27 +0100

    norminette + new functions

--- codexion.h
+++ codexion.h
@@ -1,79 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   codexion.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nunostreet <nunostreet@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/09 16:21:43 by nstreet-          #+#    #+#             */
+/*   Updated: 2026/04/09 17:46:20 by nunostreet       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef CODEXION_H
-#define CODEXION_H
+# define CODEXION_H
 
-#include <stdbool.h>
-#include <pthread.h> // mutex: init destroy lock unlock
-// threads: create join detach
-#include <stdio.h> // printf
-#include <stdlib.h> // malloc, free
-#include <unistd.h> // write, usleep
-#include <stdbool.h>
-#include <sys/time.h>
-#include <limits.h>
+# include <limits.h>
+# include <pthread.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <unistd.h>
 
-/* ANSI escape sequences for bold text colors
-Need to reset color after setting it, with RST
-Usage is: printf(R "This is red text." RST) */
+# define RST "\033[0m"
+# define BOLD_RED "\033[1;31m"
+# define BOLD_GREEN "\033[1;32m"
+# define BOLD_YELLOW "\033[1;33m"
+# define BOLD_BLUE "\033[1;34m"
+# define BOLD_MAGENTA "\033[1;35m"
+# define BOLD_CYAN "\033[1;36m"
+# define BOLD_WHITE "\033[1;37m"
 
-# define RST            "\033[0m"
-# define BOLD_RED       "\033[1;31m"
-# define BOLD_GREEN     "\033[1;32m"
-# define BOLD_YELLOW    "\033[1;33m"
-# define BOLD_BLUE      "\033[1;34m"
-# define BOLD_MAGENTA   "\033[1;35m"
-# define BOLD_CYAN      "\033[1;36m"
-# define BOLD_WHITE     "\033[1;37m"
-
-
-typedef pthread_mutex_t t_mtx;
-
-typedef struct s_reunion t_table;
+typedef pthread_mutex_t	t_mtx;
 
 typedef struct s_dongle
 {
-    t_mtx   dongle;
-    int     dongle_id;
-}   t_dongle;
+	t_mtx	dongle;
+	int		dongle_id;
+}	t_dongle;
 
-/* 
-* CODER
-*
-* ./codexion 5 800 200 200 200 [edf/fifo]
-*/
+typedef struct s_reunion
+{
+	long	number_of_coders;
+	long	time_to_burnout;
+	long	time_to_compile;
+	long	time_to_debug;
+	long	time_to_refactor;
+	long	number_of_compiles_required;
+	long	dongle_cooldown;
+	long	start_simulation;
+	char	*scheduler;
+	bool	end_simulation;
+}	t_reunion;
 
 typedef struct s_coder
 {
-    int         id;
-    long        compile_counter;
-    bool        compile;
-    long        last_compile_time; // time passed since last compile
-    t_dongle    *left_dongle;
-    t_dongle    *right_dongle;
-    pthread_t   thread_id; // a coder is a thread
-    t_reunion   *reunion;
-}   t_coder;
+	int			id;
+	long		compile_counter;
+	bool		compile;
+	long		last_compile_time;
+	t_dongle	*left_dongle;
+	t_dongle	*right_dongle;
+	pthread_t	thread_id;
+	t_reunion	*reunion;
+}	t_coder;
 
-/* 
-* TABLE to keep track of everything
-*/
-
-typedef struct s_reunion 
-{
-    long    number_of_coders; // 👨🏻‍💻
-    long    time_to_burnout;
-    long    time_to_compile;
-    long    time_to_debug;
-    long    time_to_refactor;
-    long    number_of_comiples_required;
-    long    dongle_cooldown;
-    long    start_simulation; // start timer ⏱️
-    char    *scheduler; // edf or fifo
-    bool    end_simulation; // coder burns out 🔥
-}  t_reunion;
-
-// *** prototypes ***
-
-// ** utils **
-void    error_exit(const char *error);
+void	error_exit(const char *error);
+void	error_field(const char *field, const char *error);
+long	parse_long_arg(const char *str, const char *field_name);
+void	parse_scheduler(t_reunion *reunion, char *arg);
+void	parse_input(t_reunion *reunion, char **av);
 
 #endif
