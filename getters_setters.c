d7e4283670d41e6058b859eca46fe6f6d6d4fec5
Nuno Street <nuno.dslemos@gmail.com>
2026-04-14 16:09:58 +0100

    getters and setters

---
+++ getters_setters.c
@@ -0,0 +1,42 @@
+#include "codexion.h"
+
+void	set_bool(t_mtx *mutex, bool *dest, bool value)
+{
+	safe_mutex_handle(mutex, LOCK);
+	*dest = value;
+	safe_mutex_handle(mutex, UNLOCK);
+}
+
+bool	get_bool(t_mtx *mutex, bool *value)
+{
+	bool	result;
+
+	safe_mutex_handle(mutex, LOCK);
+	result = *value;
+	safe_mutex_handle(mutex, UNLOCK);
+	return (result);
+}
+
+void	set_long(t_mtx *mutex, long *dest, long value)
+{
+	safe_mutex_handle(mutex, LOCK);
+	*dest = value;
+	safe_mutex_handle(mutex, UNLOCK);
+}
+
+long	get_long(t_mtx *mutex, long *value)
+{
+	long	result;
+
+	safe_mutex_handle(mutex, LOCK);
+	result = *value;
+	safe_mutex_handle(mutex, UNLOCK);
+	return (result);
+}
+
+void	increment_long(t_mtx *mutex, long *value)
+{
+	safe_mutex_handle(mutex, LOCK);
+	(*value)++;
+	safe_mutex_handle(mutex, UNLOCK);
+}
