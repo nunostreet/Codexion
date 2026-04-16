a2e203dd084cbb88ce0e3351af38182de7b4e8db
Nuno Street <nuno.dslemos@gmail.com>
2026-04-16 15:24:17 +0100

    C89 standards, WIP

--- codexion.h
+++ codexion.h
@@ -6,7 +6,7 @@
 /*   By: nunostreet <nunostreet@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/09 16:21:43 by nstreet-          #+#    #+#             */
-/*   Updated: 2026/04/14 15:08:31 by nunostreet       ###   ########.fr       */
+/*   Updated: 2026/04/16 15:14:11 by nunostreet       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 # include <limits.h>
 # include <pthread.h>
-# include <stdbool.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <sys/time.h>
@@ -29,6 +28,11 @@
 # define BOLD_MAGENTA "\033[1;35m"
 # define BOLD_CYAN "\033[1;36m"
 # define BOLD_WHITE "\033[1;37m"
+
+typedef	int	t_bool;
+
+# define TRUE 1
+# define FALSE 0
 
 
 typedef enum e_opcode
@@ -48,7 +52,7 @@ typedef struct s_dongle
 {
 	t_mtx	mutex;
 	int		dongle_id;
-	bool	available;
+	t_bool	available;
 	long	available_at;
 }	t_dongle;
 
@@ -57,6 +61,7 @@ typedef struct s_coder
 	int					id;
 	long				compile_counter;
 	long				last_compile_start;
+	t_bool				finished;
 	pthread_t			thread_id;
 	t_dongle			*left_dongle;
 	t_dongle			*right_dongle;
@@ -74,7 +79,8 @@ typedef struct s_reunion
 	long		dongle_cooldown;
 	long		start_simulation;
 	char		*scheduler;
-	bool		end_simulation;
+	t_bool		end_simulation;
+    t_bool		all_threads_ready;
 	t_coder		*coders;
 	t_dongle	*dongles;
 	t_mtx		write_mutex;
@@ -89,20 +95,48 @@ typedef struct s_request
 	long			seq;
 }	t_request;
 
-void	error_exit(const char *error);
-void	error_field(const char *field, const char *error);
-int		init_reunion(t_reunion *reunion);
+/* Parsing arguments */
 long	parse_long_arg(const char *str, const char *field_name);
 void	parse_scheduler(t_reunion *reunion, char *arg);
 void	parse_input(t_reunion *reunion, char **av);
+
+/* Error helpers */
+void	error_exit(const char *error);
+void	error_field(const char *field, const char *error);
+
+/* Initializing variables */
+int		init_reunion(t_reunion *reunion);
+
+/* Time functions */
 long	get_time_ms(void);
 long	elapsed_ms(long start);
-void	smart_sleep(t_reunion *reunion, long duration);
+
+/* Safe functions to handle errors */
 void	safe_mutex_handle(t_mtx *mutex, t_opcode opcode);
-void	set_bool(t_mtx *mutex, bool *dest, bool value);
-bool	get_bool(t_mtx *mutex, bool *value);
+void	safe_thread_handle(pthread_t *thread, void *(*foo)(void *), void *data,
+t_opcode opcode);
+
+/* Getters and setters to avoid repetition */
+void	set_bool(t_mtx *mutex, t_bool *dest, t_bool value);
+t_bool	get_bool(t_mtx *mutex, t_bool *value);
 void	set_long(t_mtx *mutex, long *dest, long value);
 long	get_long(t_mtx *mutex, long *value);
 void	increment_long(t_mtx *mutex, long *value);
+
+/* Simulation functions */
+void	start_simulation(t_reunion *reunion);
+void	*code_simulation(void *data);
+void	*monitor_simulation(void *data);
+
+void	wait_all_threads(t_reunion *reunion); // spinlock
+t_bool	simulation_has_ended(t_reunion *reunion);
+void	stop_simulation(t_reunion *reunion);
+
+void	print_state(t_coder *coder, const char *msg);
+void	precise_sleep(t_reunion *reunion, long duration); // needed?
+
+t_bool	try_take_dongles(t_coder *coder);
+void	release_dongles(t_coder *coder);
+void	cleanup_reunion(t_reunion *reunion);
 
 #endif
